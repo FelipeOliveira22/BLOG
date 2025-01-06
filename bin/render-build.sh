eaeb1b3b9cf5f4ac04aa48e89df327f241e944c0
FelipeOliveira22 <felipe.olv@alu.ufc.br>
2025-01-06 08:55:12 -0300

    Update buildCommand to run render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,11 +2,7 @@
 set -o errexit
 
 bundle install
+bundle exec rails db:migrate
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 
-# Use a variável DATABASE_URL do ambiente
-export DATABASE_URL=$DATABASE_URL
-
-# Rodar migrações
-bundle exec rails db:migrate
